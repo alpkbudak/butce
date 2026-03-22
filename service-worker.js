df04d1e746d49798a1ac000be39cdc4a2865a08f
alpkbudak <yunuskbudak@gmail.com>
2026-03-22 15:57:38 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'butce-v1';
+const ASSETS = [
+  '/butce/',
+  '/butce/index.html',
+  '/butce/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
